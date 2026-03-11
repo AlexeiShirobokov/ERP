33c6ac6dea310bc1891f9a29fc93da265171cbf5
AlexeiShirobokov <alexeimvc@gmail.com>
2026-03-11 15:05:47 +1000

    в settings поставил postgeres

--- ERP/settings.py
+++ ERP/settings.py
@@ -76,12 +76,28 @@ WSGI_APPLICATION = "ERP.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'django_project_db',
+            'USER': 'django',
+            'PASSWORD': 'пароль_бд',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
+
 
 
 # Password validation
